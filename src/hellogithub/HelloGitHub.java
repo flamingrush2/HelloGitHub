8815fb7532fea3b7b60075782672dbadd631d084
flamingrush2 <amolgrover2@gmail.com>
2014-12-28 15:55:07 +0530

    Initial commit

---
+++ src/hellogithub/HelloGitHub.java
@@ -0,0 +1,10 @@
+package hellogithub;
+
+/**
+ * Created by Amol on 28-12-2014.
+ */
+public class HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+    }
+}
